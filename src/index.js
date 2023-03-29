792777dcaddeac6059b79c8c0abcc23b89697fe7
st61053 <varga.matej@seznam.cz>
2023-03-29 15:05:30 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Router from "./Router";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/task-menu">
       <App >
         <Router />
       </App>
